3ec3aef73ec870fedbd2ed0ac0ca0e0832277a15
alecmarkmorrisss <ammorr28@asu.edu>
2024-11-21 12:47:45 -0700

    Adding functionality to the seller page such as a seller's book listings

--- SellerController.java
+++ SellerController.java
@@ -1,3 +1,4 @@
+//SellerController.java
 package application;
 
 
@@ -11,15 +12,37 @@ import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
 public class SellerController{
 	
 	public Books Books = new Books();
+	
 
     @FXML
     private TextField authorBox;
@@ -38,6 +61,9 @@ public class SellerController{
 
     @FXML
     private Button listMyBook;
+    
+    @FXML
+    private Button myBooksButton;
 
     @FXML
     private Button logoutButton;
@@ -50,6 +76,163 @@ public class SellerController{
     
     @FXML
     private TextField pubYearBox;
+    
+    @FXML
+    private Text titleLabel;
+    
+    @FXML
+    private Text isbnLabel;
+    
+    @FXML
+    private Text authorLabel;
+    
+    @FXML
+    private Text pubYearLabel;
+    
+    @FXML
+    private Text conditionLabel;
+    
+    @FXML
+    private Text categoryLabel;
+    
+    @FXML
+    private Text origPriceLabel;
+    
+    @FXML
+    private Button listBookButton;
+    
+    @FXML 
+    private GridPane gridPane;
+    
+    @FXML 
+    private BorderPane borderPane;
+    
+    @FXML
+    private Pane bottomPane;
+    
+    @FXML
+    private Pane topPane;
+    
+    @FXML
+    private TableView<Book> tableView;
+    @FXML
+    private TableColumn<Book, String> titleColumn;
+    
+    @FXML 
+    private TableColumn<Book, String> isbnColumn;
+    
+    @FXML
+    private TableColumn<Book, String> condColumn;
+    
+    @FXML
+    private TableColumn<Book, String> categoryColumn;
+    
+    @FXML
+    private TableColumn<Book, Double> priceColumn;
+    
+    @FXML
+    private TableColumn<Book, Boolean> soldColumn;
+    
+    @FXML
+    private void goToListBook(ActionEvent event) {
+    	System.out.println("List Book button pressed");
+    	//Make Grid Pane appear
+    	gridPane.setVisible(true);
+    	bottomPane.setVisible(true);
+    	
+    	//Make table View disappear
+    	tableView.setVisible(false);
+    	topPane.setVisible(false);
+
+		//Set MyBooks button to have a white border
+		myBooksButton.setStyle("-fx-background-color: FFC627; "
+				+ "-fx-border-radius: 20; "
+				+ "-fx-background-radius: 25;"
+				+ " -fx-border-color: #000000; "
+				+ "-fx-border-width: 2;"
+				+ " -fx-translate-x: 31;"
+				+ " -fx-translate-y: 30;");
+		
+		//List Books button to have a black border
+		listBookButton.setStyle("-fx-background-color: FFC627;"
+				+ " -fx-border-radius: 20;"
+				+ " -fx-background-radius: 25;"
+				+ " -fx-border-color: FFFFFF;"
+				+ " -fx-border-width: 2;"
+				+ " -fx-translate-x: 30;"
+				+ " -fx-translate-y: 30;");
+				
+    }
+    
+    //Function get all Books for sale by user for the table view
+    public ObservableList<Book> getBookByUser(){
+    	ObservableList<Book> userBooks = FXCollections.observableArrayList();
+		String seller = Main.thisUser.getEmail();
+    	for(int i = 0; i < Books.numBooks;i++) {
+    		if(Books.books.get(i).getSeller().equals(seller)) {
+    			userBooks.add(Books.books.get(i));	
+    		}
+    	}
+    	return userBooks;
+    }
+    
+    @FXML 
+    private void goToMyBooks(ActionEvent event) {
+    	System.out.println("My Books button pressed");
+    	//Make Grid Pane disappear
+    	gridPane.setVisible(false);
+    	bottomPane.setVisible(false);
+    	
+    	//Make table View appear
+    	tableView.setVisible(true);
+    	topPane.setVisible(true);
+    	
+        // Set items in TableView
+        System.out.println("Populating TableView...");
+
+        
+        // Set cell value factories using lambda expressions
+        titleColumn.setCellValueFactory(cellData -> 
+            new SimpleStringProperty(cellData.getValue().getTitle())
+        );
+        isbnColumn.setCellValueFactory(cellData -> 
+            new SimpleStringProperty(cellData.getValue().getIsbn())
+        );
+        condColumn.setCellValueFactory(cellData -> 
+            new SimpleStringProperty(cellData.getValue().getCondition())
+        );
+        categoryColumn.setCellValueFactory(cellData -> 
+        new SimpleStringProperty(cellData.getValue().getCategory())
+        );
+        priceColumn.setCellValueFactory(cellData -> 
+        new SimpleDoubleProperty(cellData.getValue().getGeneratedPrice()).asObject()
+        );
+        soldColumn.setCellValueFactory(cellData -> 
+        new SimpleBooleanProperty(cellData.getValue().isSold()).asObject()
+        );        
+        
+        // Populate the TableView
+        tableView.setItems(getBookByUser());
+        
+		//Set MyBooks button to have a white border
+		myBooksButton.setStyle("-fx-background-color: FFC627; "
+				+ "-fx-border-radius: 20; "
+				+ "-fx-background-radius: 25;"
+				+ " -fx-border-color: FFFFFF; "
+				+ "-fx-border-width: 2;"
+				+ " -fx-translate-x: 31;"
+				+ " -fx-translate-y: 30;");
+		
+		//List Books button to have a black border
+		listBookButton.setStyle("-fx-background-color: FFC627;"
+				+ " -fx-border-radius: 20;"
+				+ " -fx-background-radius: 25;"
+				+ " -fx-border-color: #000000;"
+				+ " -fx-border-width: 2;"
+				+ " -fx-translate-x: 30;"
+				+ " -fx-translate-y: 30;");
+    
+    }
 
     @FXML
     private void listMyBookButton(ActionEvent event){
@@ -70,7 +253,7 @@ public class SellerController{
     	//Call to checkIfEmpty function
     	if(inputIsMissing(title, isbn, author, condition, category,originalPrice, pubYear) == false) {
         	Books.addBook(isbn, title, author, condition, category, Double.valueOf(originalPrice), generatedPrice(condition, Double.valueOf(originalPrice)) , seller, "", false, Integer.parseInt(pubYear));
-        	//Testing purposes
+        	/*Testing purposes
         	System.out.println("Title: " + title );
         	System.out.println("ISBN: " + isbn );
         	System.out.println("Author: " + author);
@@ -82,7 +265,7 @@ public class SellerController{
         	System.out.println("Year Published: " + pubYear);
         	System.out.println("This is should be after book is added");
         	Books.printBooks();
-        	
+        	*/
         	
     		//Alerting the user that the book has been posted for sale
         	Alert bookPosted= new Alert(AlertType.CONFIRMATION);
@@ -128,8 +311,12 @@ public class SellerController{
     	return false;
     }
 
+
+    
     @FXML
     private void logOut(ActionEvent event) throws Exception {
+    	
+    	
     	System.out.println("Logout Button pressed");
     	Node node = (Node)event.getSource();
     	Stage stage = (Stage)node.getScene().getWindow();
@@ -142,6 +329,11 @@ public class SellerController{
     
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() throws FileNotFoundException {
+
+
+
+    	
+    	
     	//Setting combo boxes with data that be selected from user
     	categoryBox.setItems(categories);
     	conditionBox.setItems(conditions);
@@ -150,6 +342,8 @@ public class SellerController{
     	System.out.println("SellerController Initialized");
     	System.out.println("Number Of Books: " + Books.getNumBooks());
     	Books.uploadBooks();
+    	
+    	System.out.println("\nPrinting Books:");
     	Books.printBooks();
     	
     	//Event Filter that only allows Characters into the Author Field
@@ -207,6 +401,7 @@ public class SellerController{
                 }
             }
         });
+    	
     }
 
 }
